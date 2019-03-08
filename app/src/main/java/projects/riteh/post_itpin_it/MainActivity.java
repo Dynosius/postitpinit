5955b443b546665ceb3e1c4e7072e1f5202f2abd
Ivana Zuzic <izuzic3@gmail.com>
2019-03-08 15:32:57 +0100

    Added three tabs to MainActivity

--- app/src/main/java/projects/riteh/post_itpin_it/MainActivity.java
+++ app/src/main/java/projects/riteh/post_itpin_it/MainActivity.java
@@ -1,19 +1,24 @@
 package projects.riteh.post_itpin_it;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
-
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new Tab2Fragment(), "Tab 2");
+        adapter.addFragment(new Tab3Fragment(), "Tab 3");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
-
-
 }
---
+++ app/src/main/java/projects/riteh/post_itpin_it/Tab1Fragment.java
@@ -0,0 +1,12 @@
+package projects.riteh.post_itpin_it;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class Tab1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
