8dfadc8ce706cd7ab09a2a105db2be7311eecff3
Ivana Zuzic <izuzic3@gmail.com>
2019-06-02 22:08:18 +0200

    Swipeable messages inside the widget This still needs improvement since the messages in the widget are hardcoded.

--- app/src/main/java/projects/riteh/post_itpin_it/view/MyWidgetProvider.java
+++ app/src/main/java/projects/riteh/post_itpin_it/view/MyWidgetProvider.java
@@ -8,11 +8,13 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import projects.riteh.post_itpin_it.R;
+import projects.riteh.post_itpin_it.service.WidgetService;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
@@ -27,12 +29,19 @@ public class MyWidgetProvider extends AppWidgetProvider {
                 MyWidgetProvider.class);
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
         for (int widgetId : allWidgetIds) {
+
+            Intent serviceIntent = new Intent(context, WidgetService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
             Intent configIntent = new Intent(context, TransparentPostit.class);
 
             PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
 
             remoteViews.setOnClickPendingIntent(R.id.button, configPendingIntent);
+            remoteViews.setRemoteAdapter(R.id.widget_stack_view, serviceIntent);
+            remoteViews.setEmptyView(R.id.widget_stack_view, R.id.widget_empty_stack_view);
 
             appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
         }
